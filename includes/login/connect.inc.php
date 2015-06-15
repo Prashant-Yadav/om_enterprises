189a40907fbc7536ac882579f1403d4b01f5517d
Prashant-Yadav <prashantyadav.indore@gmail.com/>
2015-06-15 13:09:24 +0530

    name of files at some places updated

--- includes/login/connect.inc.php
+++ includes/login/connect.inc.php
@@ -2,7 +2,7 @@
 	$host = 'localhost';
 	$user = 'root';
 	$pass = '';
-	$db = 'jatin_enterprises';
+	$db = 'om_enterprises_db';
 	$con = mysqli_connect($host,$user,$pass,$db);
 	if(mysqli_connect_errno($con)){
 		echo 'Failed to connect to the database : '.mysqli_connect_error();
--- includes/logout.php
+++ includes/logout.php
@@ -2,5 +2,5 @@
 	require 'login/core.inc.php';
 	session_destroy();
 	//echo $httpReferer;
-	header('Location:'.$httpReferer);
+	header('Location:'.$httpReferer);   //this will redirect to page from which user logout
 ?>
