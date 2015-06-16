552f1597d86c03c6a98c3bd3d777912bc0c33b4d
Prashant-Yadav <prashantyadav.indore@gmail.com/>
2015-06-16 12:47:23 +0530

    upadated UI, included new files for UI, included new file for item addition, modified navigation menu

---
+++ add_item.php
@@ -0,0 +1,126 @@
+<!-- Including files for DB connection and Session Control -->
+<?php
+    include 'includes/login/core.inc.php';
+    include 'includes/login/connect.inc.php';
+?>
+<!-- /End of includes -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php
+    if(loggedIn()){ #This function is in /includes/login/core.inc.pho for verfying user session
+
+    	if (isset($_POST['submitItem'])) {
+    		# insert new item details
+    		if (!empty($_POST['brandName'])) {
+    			# code...
+    			$brandName = $_POST['brandName'];
+    			$itemDimension = $_POST['itemDimension'];
+    			$itemThickness = $_POST['itemThickness'];
+    			$availableStock = $_POST['availableStock'];
+
+    			$queryRun = mysqli_query($con, "INSERT INTO `brand`(`brand_no`, `brand_name`, `dimension`, `thickness`, `stock`) VALUES ( '', '$brandName', '$itemDimension', '$itemThickness', '$availableStock')");
+    		}
+    	}
+
+  ?>
+
+	<head>
+	      <meta charset="utf-8">
+	      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	      <meta name="viewport" content="width=device-width, initial-scale=1">
+	      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	      <title>Add New Item</title>
+
+	      <!-- Bootstrap core css -->
+	      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	      <!-- Custom CSS -->
+	      <link href="assets/css/sb-admin.css" rel="stylesheet">
+
+	      <!-- Morris Charts CSS -->
+	      <link href="assets/css/plugins/morris.css" rel="stylesheet">
+
+	      <!-- Custom Fonts -->
+	      <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+	      <!-- Custom styles for this template -->
+	      <link href="assets/css/justified-nav.css" rel="stylesheet">
+
+	      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	      <!--[if lt IE 9]>
+	        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	      <![endif]-->
+	</head>
+	<body>
+	
+	    <!-- code to include navigation menu-->
+	      <div id="wrapper">
+	      
+	        <?php require 'includes/menu_bar.inc.php';?>  
+	      
+	        <!-- div for main pages-->
+	        <div id="page-wrapper">
+	        	<h1>
+	        		Add new items
+	        	</h1>
+
+	        	<form action="#" method="POST" class="form-inline">
+	        		<pre>
+	        			<strong>Brand Name</strong>			<input type="text" class="form-control" placeholder="name" name="brandName" id="brandName">
+	        			<strong>Dimension</strong>			<select class="form-control" name="itemDimension" id="itemDimension">
+	        													<option value="8x4">8x4</option>
+	        													<option value="8x4">8x3</option>
+	        													<option value="7x4">7x4</option>
+	        													<option value="7x3">7x3</option>
+	        													<option value="6x4">6x4</option>
+	        													<option value="6x3">6x3</option>
+	        												</select>
+	        			<strong>Thickness</strong>			<select class="form-control" name="itemThickness" id="itemThickness">
+	        													<option value="4mm">4mm</option>
+	        													<option value="6mm">6mm</option>
+	        													<option value="8mm">8mm</option>
+	        													<option value="12mm">12mm</option>
+	        													<option value="15mm">15mm</option>
+	        													<option value="16mm">16mm</option>
+	        													<option value="18mm">18mm</option>
+	        													<option value="19mm Blockboard">19mm Blockboard</option>
+	        													<option value="25mm board">25mm board</option>
+	        												</select>
+	        			<strong>Available Stock</strong>	<input type="text" class="form-control" placeholder="0" name="availableStock" id="availableStock">
+	        			<br>
+	        			<input type="submit" class="btn btn-default" label="Submit" value="Submit" id="submitItem" name="submitItem">	
+	        		</pre>   		
+	        	</form>
+	        </div>  <!-- /#page-wrapper-->
+      
+			</div> <!-- /#wrapper -->
+
+
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+			<!-- jQuery -->
+			<script src="assets/js/jquery.js"></script>
+
+			<!-- Include all compiled plugins (below), or include individual files as needed -->
+			<script src="assets/js/bootstrap.min.js"></script>
+
+			<!-- Morris Charts JavaScript -->
+			<script src="../assets/js/plugins/morris/raphael.min.js"></script>
+			<script src="../assets/js/plugins/morris/morris.min.js"></script>
+			<script src="../assets/js/plugins/morris/morris-data.js"></script>
+
+  <?php
+    } #End of LoggedIn function
+    else{
+        #redired to loginform if not loggedIn
+        include 'includes/login/loginform.inc.php' ;
+    }
+  ?>
+
+	</body>
+</html>
---
+++ includes/menu_bar.inc.php
@@ -0,0 +1,43 @@
+<style type="text/css">
+    .date {
+        background-color: white;
+    }
+</style>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <a class="navbar-brand" href="index.html">Om Enterprises</a>
+    </div>
+
+    <!-- Top Menu items -->
+    <ul class="nav navbar-right top-nav">
+        <li>
+            <a href="#">
+              <?php
+                echo date("d/m/Y");
+              ?>  
+            </a>
+        </li>
+        <li>
+            <a href="includes/logout.php">Logout</a>
+        </li>
+    </ul>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav side-nav">
+            <li class="active">
+                <a href="../index.php"><i class="fa fa-fw fa-dashboard"></i> Home</a>
+            </li>
+            <li>
+                <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+            </li>
+            <li>
+                <a href="add_item.php"><i class="fa fa-fw fa-table"></i> Add New Item</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+</nav>
